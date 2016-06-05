af719824f58631e4279f61da62c0e94d2961a5fc
bharding2 <b.harding@gmail.com>
2016-06-05 12:01:28 -0700

    hash methods

--- models/user.js
+++ models/user.js
@@ -5,7 +5,16 @@ const jwt = require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  findHash: { type: String, unique: true }
 });
+
+userSchema.methods.generateHash = function(password) {
+  return this.password = bcrypt.hashSync(password, 8);
+};
+
+userSchema.methods.compareHash = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = exports = mongoose.model('User', userSchema);
