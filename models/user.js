245522ebc8df555e0f47dda7ed496494db112d8b
bharding2 <b.harding@gmail.com>
2016-06-05 12:06:48 -0700

    generate find hash function

--- models/user.js
+++ models/user.js
@@ -17,4 +17,28 @@ userSchema.methods.compareHash = function(password) {
   return bcrypt.compareSync(password, this.password);
 };
 
+userSchema.methods.generateFindHash = function(cb) {
+  var tries = 0;
+  var timeout;
+  var _generateFindHash = () => {
+    var hash = crypto.randomBytes(32);
+    this.findHash = hash.toString('hex');
+    this.save((err) => {
+      if (err) {
+        if (tries > 9) {
+          return cb(new Error('could not generate hash'));
+        }
+        return timeout = setTimeout(() => {
+          _generateFindHash();
+          tries++;
+        }, 1000);
+      }
+
+      if (timeout) clearTimeout(timeout);
+      cb(null, hash.toString('hex'));
+    });
+  };
+  _generateFindHash();
+};
+
 module.exports = exports = mongoose.model('User', userSchema);
