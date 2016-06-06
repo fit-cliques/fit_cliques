899375ea32196c681f5cdac13237e74d1226f365
bharding2 <b.harding@gmail.com>
2016-06-05 20:12:16 -0700

    test setup

---
+++ test/test_setup.js
@@ -0,0 +1,12 @@
+process.env.MONGDOB_URI = 'mongodb://localhost/fit_cliques_testDB';
+const mongoose = require('mongoose');
+const port = process.env.PORT = 5050;
+const server = require(__dirname + '/../server');
+module.exports = exports = (cb) => {
+  mongoose.connect(process.env.MONGODB_URI, () => {
+    server.listen(port, () => {
+      console.log('server up on port: ' + port);
+      cb();
+    });
+  });
+};
