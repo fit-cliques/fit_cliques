056eb1e71c5712a4aec31b266f5e752c75e072ee
bharding2 <b.harding@gmail.com>
2016-06-05 18:22:53 -0700

    signup route

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -3,3 +3,25 @@ const bodyParser = require('body-parser').json();
 const basicHTTP = require(__dirname + '/../lib/basic_http');
 const User = require(__dirname + '/../models/user');
 const jwtAuth = require(__dirname + '/../lib/jwt_auth');
+
+var authRouter = module.exports = exports = Router();
+
+authRouter.post('/signup', bodyParser, (req, res) => {
+  var password = req.body.password;
+  req.body.password = null;
+
+  if (!password) return res.status(500).json({ msg: 'no blank passwords' });
+
+  var newUser = new User(req.body);
+  newUser.generateHash(password);
+  password = null;
+
+  newUser.save((err, user) => {
+    if (err) return res.status(500).json({ msg: 'could not create user' });
+
+    user.generateToken((err, token) => {
+      if (err) return res.status(500).json({ msg: 'could not generate token' });
+      res.json({ token });
+    });
+  });
+});
