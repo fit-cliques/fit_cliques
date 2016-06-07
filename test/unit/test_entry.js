578418f3e425e6196877b1c2398332bc77c2f9bd
bharding2 <b.harding@gmail.com>
2016-06-07 11:43:36 -0700

    sign in controller test

--- test/unit/test_entry.js
+++ test/unit/test_entry.js
@@ -4,4 +4,5 @@ require('../../app/js/entry.js');
 require('./user_controller_test.js');
 require('./auth_controller_test.js');
 require('./signup_controller_test.js');
+require('./signin_controller_test.js');
 require('./fb_user_auth_test');
