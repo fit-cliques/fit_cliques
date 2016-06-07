aed1995e5930f118d250edffca33debc8979a999
bharding2 <b.harding@gmail.com>
2016-06-07 08:10:18 -0700

    auth service

--- app/js/auth/services/auth_service.js
+++ app/js/auth/services/auth_service.js
@@ -5,10 +5,34 @@ var config = require('../../config');
 module.exports = function(app) {
   app.factory('fitCliqueAuth', ['$http', '$q', function($http, $q) {
     return {
-      removeToken: function() {},
-      saveToken: function() {},
-      getToken: function() {},
-      getUsername: function() {}
+      removeToken: function() {
+        this.token = null;
+        this.username = null;
+        $http.defaults.headers.common.token = null;
+        window.localStorage.token = '';
+      },
+      saveToken: function(token) {
+        this.token = token;
+        $http.defaults.headers.common.token = token;
+        window.localStorage.token = token;
+        return token;
+      },
+      getToken: function() {
+        this.token || this.saveToken(window.localStorage.token);
+        return this.token;
+      },
+      getUsername: function() {
+        return $q(function(resolve, reject) {
+          if (this.username) return resolve(this.username);
+          if (!this.getToken()) return reject(new Error('no app auth token'));
+
+          $http.get(config.baseUrl + '/api/profile')
+            .then((res) => {
+              this.username = res.data.username;
+              resolve(res.data.username);
+            }, reject);
+        }.bind(this));
+      }
     };
   }]);
 };
