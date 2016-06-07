b6418a7ef2da2f19c8714fe171122dda486c8613
bharding2 <b.harding@gmail.com>
2016-06-07 11:35:28 -0700

    signup controller test

--- test/unit/test_entry.js
+++ test/unit/test_entry.js
@@ -3,4 +3,5 @@ require('angular-mocks');
 require('../../app/js/entry.js');
 require('./user_controller_test.js');
 require('./auth_controller_test.js');
+require('./signup_controller_test.js');
 require('./fb_user_auth_test');
