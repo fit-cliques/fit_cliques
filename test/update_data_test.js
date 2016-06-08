1ccff562932556206870181d601acf57dcea035f
Greg Magdsick <greg.magdsick@gmail.com>
2016-06-08 15:50:57 -0700

    made notes on test

---
+++ test/update_data_test.js
@@ -0,0 +1,8 @@
+// create test DB
+// create 2 test users
+// capture fitBit API requests
+//   return mock data
+// check values on user objects
+// check http://robdodson.me/mocking-requests-with-mocha-chai-and-sinon/
+// and http://sinonjs.org
+//         for more info on how to do this
