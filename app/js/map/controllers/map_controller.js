4ee7eb656ed2af47ab0b9ce55e892de6f4c5f133
bharding2 <b.harding@gmail.com>
2016-06-09 11:02:07 -0700

    centering map and placing markers

--- app/js/map/controllers/map_controller.js
+++ app/js/map/controllers/map_controller.js
@@ -1,11 +1,44 @@
+var config = require('../../config');
+const async = require('async');
+
 module.exports = function(app) {
-  app.controller('MapController', function() {
+  app.controller('MapController', ['$http', 'fbUserAuth', function($http, fbUserAuth) {
     var mapEle = document.getElementById('map');
-    var mapOptions = {
-      zoom: 8,
-      center: new google.maps.LatLng(47.608013, -122.335167),
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(mapEle, mapOptions);
-  });
+
+
+    $http.get(config.baseUrl + '/api/zipcode')
+      .then(function(res) {
+        var zipCodes = res.data;
+
+        var uniqueZipcodes = Object.keys(zipCodes);
+
+        $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' +
+        98144 + '+usa')
+        .then(function(res) {
+          var latlng = new google.maps.LatLng(res.data.results[0].geometry.location.lat,
+            res.data.results[0].geometry.location.lng);
+          var mapOptions = {
+            zoom: 8,
+            center: latlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+          };
+
+          this.map = new google.maps.Map(mapEle, mapOptions);
+
+          uniqueZipcodes.forEach((ele) => {
+            $http.get('http://maps.googleapis.com/maps/api/geocode/json?address=' +
+            ele + 'usa')
+            .then((res) => {
+              var zipLatLng = new google.maps.LatLng(res.data.results[0].geometry.location.lat,
+                res.data.results[0].geometry.location.lng);
+              var marker = new google.maps.Marker({
+                position: zipLatLng,
+                map: this.map,
+                title: 'Hello World!'
+              });
+            });
+          });
+        });
+      });
+  }]);
 };
