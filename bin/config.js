222f2572543b9aee535aa11e464b734e8bbb313f
bharding2 <b.harding@gmail.com>
2016-06-10 09:49:16 -0700

    bin config

---
+++ bin/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  baseUrl: 'https://fit-cliques.herokuapp.com',
+  fbAuthUrl: 'https://api.fitbit.com/oauth2/token',
+  fbDataUrl: 'https://api.fitbit.com/1/user/'
+};
--- bin/update_data.js
+++ bin/update_data.js
@@ -1,4 +1,4 @@
-const config = require('../app/js/config');
+const config = require('./config');
 const mongoose = require('mongoose');
 const update = require('./_update_data');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fit_cliques_DB');
